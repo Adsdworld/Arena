af1e83b669b70f1d8cc6dd5ffe334b52efd70aa3
Benetreau <arthur.benetreau@reseau.eseo.fr>
2025-06-04 17:31:24 +0200

    WIP on benetreau
    Résolution de conflit normalement c bon

---
+++ Assets/Script/Ui/menu/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections ; 
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Ui.menu
+{
+    public class Health : MonoBehaviour
+    {
+        public float health = 75f; 
+        public float maxHealth = 100f; 
+    
+        public Image healthBar; 
+    
+    
+        // Update is called once per frame 
+        void Update()
+        {
+            healthBar.fillAmount = health / maxHealth;
+        }
+    }
+}
