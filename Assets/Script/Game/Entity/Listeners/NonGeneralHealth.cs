aea57666bc8bef8a6a12618bb1d43da765d612cf
A.SALLIER <sallierdupin.arthur.pro@gmail.com>
2025-06-14 19:20:10 +0200

    Adds health bar to entities
    Implements a health bar UI element for entities.
    
    The health bar is displayed above the entity and shows the current health as a percentage of the maximum health.
    The health bar is only displayed for non-local and living entities.
    The scale of the EntityBase prefab was increased for better visibility of the health bar.

---
+++ Assets/Script/Game/Entity/Listeners/NonGeneralHealth.cs
@@ -0,0 +1,52 @@
+﻿using Script.Game.Player;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Game.Entity.Listeners
+{
+    public class Health : MonoBehaviour
+    {
+        public float health; 
+        public float maxHealth; 
+    
+        public Image healthBar; 
+        [SerializeField] private EntityComponent _entityComponent;
+        [SerializeField] private GameObject canvasRoot;
+
+        
+        void Awake()
+        {
+            _entityComponent = GetComponentInParent<EntityComponent>();
+            if (healthBar == null)
+            {
+                healthBar = GetComponent<Image>(); // ce script est sur l’image rouge
+            }
+            canvasRoot = GetComponentInParent<Canvas>().gameObject;
+        }
+        
+        void Update()
+        {
+            if (_entityComponent.IsUnityNull()) return;
+
+            health = _entityComponent.Health;
+            maxHealth = _entityComponent.MaxHealth;
+
+            bool isLocal = "Entity_" + _entityComponent.Id == LocalPlayer.Instance.GetControlledEntityId();
+            bool isDead = health <= 0;
+            canvasRoot.SetActive(!(isLocal || isDead));
+
+            if (isLocal) return;
+
+            if (maxHealth > 0)
+            {
+                healthBar.fillAmount = Mathf.Clamp01(health / maxHealth);
+            }
+            else
+            {
+                healthBar.fillAmount = 0;
+            }
+        }
+
+    }
+}
