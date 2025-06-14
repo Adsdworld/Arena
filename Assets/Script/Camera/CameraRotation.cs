45f883f1954ce5f5adf01fdafbb31d21c227a4c1
A.SALLIER <sallierdupin.arthur.pro@gmail.com>
2025-06-14 14:59:13 +0200

    Rotation according to team

--- Assets/Script/Camera/CameraRotation.cs
+++ Assets/Script/Camera/CameraRotation.cs
@@ -5,17 +5,18 @@ namespace Script.Camera
 {
     public class CameraRotation
     {
-        private readonly Vector3 team1Angles = new Vector3(50f, 35f, 0f);
-        private readonly Vector3 team2Angles = new Vector3(50f, 215f, 0f);
+        private readonly Vector3 _team1Angles = new Vector3(50f, 35f, 0f);
+        private readonly Vector3 _team2Angles = new Vector3(50f, 215f, 0f);
+        private readonly Vector3 _team0Angles = new Vector3(90f, 0f, 0f);
 
         public Vector3 GetRotation()
         {
-            int team = LocalPlayer.Instance?.Team ?? 1;
+            int team = LocalPlayer.Instance.GetControlledEntityComponent().Team ;
             return team switch
             {
-                1 => team1Angles,
-                2 => team2Angles,
-                _ => team1Angles
+                1 => _team1Angles,
+                2 => _team2Angles,
+                _ => _team0Angles
             };
         }
     }
